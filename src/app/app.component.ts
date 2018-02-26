1c1f186ab49303aa66ba3e8e4c6caa1d25bbc53c
valePV <va.perez19@hotmail.com>
2018-02-26 18:04:01 -0300

    ejercicios en clases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Cadaver Exquisito';
+  poemas = [
+  	true,
+  	true
+  ];
+
+  onPoemMostrar(input){
+  	this.poemas[input.id]=input.mostrar;
+  }
 }
